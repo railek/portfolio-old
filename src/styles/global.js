5e8498bba3b3e3c9958d673e00f57661e3e10b2b
railek <jean@railek.com>
2020-01-02 16:30:28 +0200

    style: custom webkit scrollbar

--- src/styles/global.js
+++ src/styles/global.js
@@ -8,6 +8,23 @@ const GlobalStyle = createGlobalStyle`
   ${Reset}
   ${Color}
   ${Typography}
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: var(--gray-700);
+  }
+
+  *::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: var(--gray-900);
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: var(--gray-700);
+  }
 `;
 
 export default GlobalStyle;
